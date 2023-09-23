6cb3a2b601fb09d505f5cdb7790dcfc265b54eb1
rg-16 <roger.gonzales.884@gmail.com>
2023-09-22 22:18:55 -0400

    Create Provider page.

---
+++ src/pages/Providers/index.tsx
@@ -0,0 +1,30 @@
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import ProviderList from 'src/components/Provider/ListProvider'
+import InsertPovider from 'src/components/Provider/InsertProvider'
+
+const Provider = () => {
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Typography variant='h5'>
+            Lista de Proveedores
+        </Typography>
+        <InsertPovider/>
+      </Grid>
+  
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Proveedores' titleTypographyProps={{ variant: 'h6' }} />
+          <ProviderList/>
+        </Card>
+      </Grid>
+
+    </Grid>
+  )
+}
+
+export default Provider
