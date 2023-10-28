ca213586562280b0292401933723508416ea2371
Freddy Quiroga <freddybalderrama.fa@gmail.com>
2023-10-27 21:29:27 -0400

    added insert sale

--- src/components/Sale/InsertSale/index.tsx
+++ src/components/Sale/InsertSale/index.tsx
@@ -8,217 +8,169 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import { useTheme } from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
-import FormControl from '@mui/material/FormControl'
-import InputLabel from '@mui/material/InputLabel'
-import Select from '@mui/material/Select'
-import MenuItem from '@mui/material/MenuItem'
+
+import Paper from '@mui/material/Paper'
+import Table from '@mui/material/Table'
+import TableRow from '@mui/material/TableRow'
+import TableHead from '@mui/material/TableHead'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+
 import { useState } from 'react'
-import { useAddNewProductMutation } from 'src/api/Product'
+import { useAddNewProductMutation, useGetProductByNameQuery } from 'src/api/Product'
 import Link from '@mui/material/Link'
 import CloudUploadIcon from 'mdi-material-ui/CloudUpload'
 import { styled } from '@mui/material/styles'
+import { Autocomplete, IconButton } from '@mui/material'
+import Delete from 'mdi-material-ui/Delete'
+import { useGetAllClientQuery } from 'src/api/clientApi'
+
 
 const InsertSales = () => {
-
   const [open, setOpen] = useState(false)
   const theme = useTheme()
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
 
-  const initialValuesInputs = {
-    idProducto: 80,
-    nombreProducto: '',
-    categoria: '1',
-    codigo: '',
-    imagen: '',
-    marca: '',
-    tipo: 'MT',
-    descripcion: '',
-    alto: 0,
-    ancho: 0,
-    espesor: '',
-    precioVenta: '',
-    estado: 1,
+  interface Product {
+    idProducto: number
+    categoria: number
+    codigo: string
+    imagen: string
+    nombreProducto: string
+    precioVenta: number
+    estado: number
+    alto: number | null
+    ancho: number | null
+    espesor: number | null
+    marca: string
+    tipo: string
+    Descripcion: string
   }
-  const [inputsValues, setinputsValues] = useState(initialValuesInputs);
-
-  const handleInputChange = (event: any) => {
-    const { name, value } = event.target
-    setinputsValues({
-      ...inputsValues,
-      [name]: value
-    })
+  interface Client {
+    nitCi: string;
+    businessName: string;
+    phoneNumber: number;
   }
-
   const handleClickOpen = () => {
     setOpen(true)
   }
 
   const handleClose = () => {
-    setOpen(false);
-    setinputsValues(initialValuesInputs);
+    setOpen(false)
   }
 
-  const [addNewProduct, { isLoading, isError }] = useAddNewProductMutation();
+  const [productName, setProductName] = useState('a')
+  const { data: products, isError, error } = useGetProductByNameQuery(productName);
+  const { data: clients, isLoading} = useGetAllClientQuery();
 
 
-  const handleAddProduct = async () => {
-    try {
-      await addNewProduct(inputsValues).unwrap()
-      handleClose()
-      setinputsValues(initialValuesInputs)
-    } catch (error) {
-      console.log(error)
-    }
+  const handleInputChange = (event: any, value: any) => {
+    setProductName(value)
   }
-  const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1
-  });
 
-  const handleFileChange = ( event:any ) => { 
-    const file = event.target.files[0];
-    const reader = new FileReader();
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([])
 
-    reader.onload = () => {
-      setinputsValues( { 
-        ...inputsValues,
-        imagen: reader.result as string
-       })
-    };
-    reader.readAsDataURL( file );
-
-  }
   return (
     <div>
-      <Button variant='outlined' onClick={ handleClickOpen }>
+      <Button variant='outlined' onClick={handleClickOpen}>
         Insertar Venta
       </Button>
       <Dialog fullScreen={fullScreen} open={open} onClose={handleClose} aria-labelledby='responsive-dialog-title'>
         <DialogTitle id='responsive-dialog-title'>
           {' '}
-          <Link href='#'> Añadir una nueva Venta</Link>
+          <Link href='#'> Añadir una nueva venta</Link>
         </DialogTitle>
         <DialogContent>
           <Grid container spacing={7} style={{ paddingTop: '5px' }}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name='nombreProducto'
-                value={inputsValues.nombreProducto}
-                onChange={handleInputChange}
-                fullWidth
-                label='Nombre'
+
+            <Grid item xs={6} sm={6}>
+              <Autocomplete
+                options={clients ? clients : []}
+                getOptionLabel={(option: Client) => option.businessName}
+                renderInput={params => (
+                  <TextField
+                    {...params}
+                    label='Buscar Cliente'
+                    variant='outlined'
+                  />
+                )}
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Categoria</InputLabel>
-                <Select
-                  label='Categoria'
-                  name='categoria'
-                  onChange={handleInputChange}
-                  value={inputsValues.categoria}
-                >
-                  <MenuItem value='1'>Accesorios</MenuItem>
-                  <MenuItem value='2'>Aceites</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name='codigo'
-                value={inputsValues.codigo}
-                onChange={(e: any) => handleInputChange(e)}
-                fullWidth
-                label='Cod I'
+            <Grid item xs={6} sm={6}>
+              <Autocomplete
+                options={products ? products : []}
+                getOptionLabel={(option: Product) => option.nombreProducto}
+                onChange={(event, value): void => {
+                  if (value) {
+                    setSelectedProducts(prevSelectedProducts => [...prevSelectedProducts, value])
+                  }
+                }}
+                renderInput={params => (
+                  <TextField
+                    {...params}
+                    label='Buscar producto'
+                    variant='outlined'
+                    onChange={e => handleInputChange(e, e.target.value)}
+                  />
+                )}
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl fullWidth>
-                <InputLabel>Tipo</InputLabel>
-                <Select label='Role' name='tipo' onChange={handleInputChange} value={ inputsValues.tipo }>
-                  <MenuItem value='MT'>MT</MenuItem>
-                  <MenuItem value='AT'>AT</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name='marca'
-                value={inputsValues.marca}
-                onChange={handleInputChange}
-                fullWidth
-                type='text'
-                label='Marca'
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                name='descripcion'
-                value={inputsValues.descripcion}
-                onChange={handleInputChange}
-                fullWidth
-                id='outlined-multiline-static'
-                label='Descripcion'
-                multiline
-                rows={2}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                type='number'
-                label='Alto'
-                name='alto'
-                value={inputsValues.alto}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                type='number'
-                label='Ancho'
-                name='ancho'
-                value={inputsValues.ancho}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label='Espesor'
-                name='espesor'
-                value={inputsValues.espesor}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                type='number'
-                label='Precio Venta'
-                name='precioVenta'
-                value={inputsValues.precioVenta}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Button component='label' variant='contained' startIcon={<CloudUploadIcon />}>
-                Subir Imagen
-                <VisuallyHiddenInput type='file' onChange={handleFileChange} />
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              {inputsValues.imagen && 
-                  <img src={inputsValues.imagen} alt='Preview' style={{ maxWidth: '100%', height: 'auto' }} />
-              }
+            <Grid item xs={12} sm={12}>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} stickyHeader aria-label='sticky table'>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Imagen</TableCell>
+                      <TableCell>Cod I</TableCell>
+                      <TableCell align='right'>Nombre</TableCell>
+                      <TableCell align='right'>Tipo </TableCell>
+                      <TableCell align='right'>Categoria</TableCell>
+                      <TableCell align='right'>Marca </TableCell>
+                      <TableCell align='right'>Descripcion </TableCell>
+                      <TableCell align='right'>Alto</TableCell>
+                      <TableCell align='right'>Ancho</TableCell>
+                      <TableCell align='right'>Esp</TableCell>
+                      <TableCell align='right'>Eliminar</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {selectedProducts.map(product => (
+                      <TableRow key={product.idProducto}>
+                        <TableCell>
+                          <img
+                            src={`/images/products/${product.imagen}`}
+                            alt={product.nombreProducto}
+                            style={{ maxWidth: '100%', height: '80px' }}
+                          />
+                        </TableCell>
+                        <TableCell>{product.codigo}</TableCell>
+                        <TableCell align='right'>{product.nombreProducto}</TableCell>
+                        <TableCell align='right'>{product.tipo}</TableCell>
+                        <TableCell align='right'>{product.categoria}</TableCell>
+                        <TableCell align='right'>{product.marca}</TableCell>
+                        <TableCell align='right'>{product.Descripcion}</TableCell>
+                        <TableCell align='right'>{product.alto}</TableCell>
+                        <TableCell align='right'>{product.ancho}</TableCell>
+                        <TableCell align='right'>{product.espesor}</TableCell>
+                        <TableCell align='right'>
+                      
+
+                          <IconButton aria-label='Delete' color='error' onClick={() => {
+                              // Handle delete or remove logic here
+                              setSelectedProducts(prevSelectedProducts =>
+                                prevSelectedProducts.filter(p => p.idProducto !== product.idProducto)
+                              )
+                            }}>
+                            <Delete />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                    {/* ... Other table rows ... */}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </Grid>
           </Grid>
         </DialogContent>
@@ -226,10 +178,10 @@ const InsertSales = () => {
           <Button autoFocus onClick={handleClose}>
             Cancelar
           </Button>
-          <Button onClick={handleAddProduct} disabled={isLoading} variant='contained' autoFocus>
+          {/* <Button onClick={handleAddProduct} disabled={isLoading} variant='contained' autoFocus>
             { isLoading ? 'Añadiendo venta...' : 'Añadir venta' }
-          </Button>
-          {isError && <div> Error al añadir una venta </div>}
+          </Button> */}
+          {isError && <div> Error al buscar un producto </div>}
         </DialogActions>
       </Dialog>
     </div>
